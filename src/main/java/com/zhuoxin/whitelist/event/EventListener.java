52fd45678e8d8120146da920ade0ddeb489c03b8
zhuoxin-lzk <zhuoxin.lzk@gmail.com>
2024-03-13 20:17:52 +0800

    修改公告字体颜色

--- src/main/java/com/zhuoxin/whitelist/event/EventListener.java
+++ src/main/java/com/zhuoxin/whitelist/event/EventListener.java
@@ -2,6 +2,7 @@ package com.zhuoxin.whitelist.event;
 
 import com.zhuoxin.whitelist.util.dbOperate;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
@@ -14,7 +15,7 @@ public class EventListener implements Listener {
         //获取玩家游戏名称
         String loggingPlayerName = loggingEvent.getName();
         //公告该玩家尝试加入世界
-        Bukkit.getServer().broadcastMessage(loggingPlayerName + " is trying to joining the world");
+        Bukkit.getServer().broadcastMessage(ChatColor.YELLOW + loggingPlayerName + " is trying to joining the world");
         //如果该玩家在白名单中
         if (dbOperate.isNameExists(loggingPlayerName)) {
             //允许加入
@@ -25,7 +26,7 @@ public class EventListener implements Listener {
             //拒绝加入并提示原因
             loggingEvent.disallow(AsyncPlayerPreLoginEvent.Result.KICK_WHITELIST, "you are not in whitelist");
             //公告该玩家不在白名单
-            Bukkit.getServer().broadcastMessage("But " + loggingPlayerName + " is not in the whitelist");
+            Bukkit.getServer().broadcastMessage(ChatColor.YELLOW + "But " + loggingPlayerName + " is not in the whitelist");
         }
     }
 }
