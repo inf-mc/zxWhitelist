f7f4116a800af7e3b231f4b13d543ce51b09828c
zhuoxin-lzk <zhuoxin.lzk@gmail.com>
2024-03-13 16:10:26 +0800

    细节优化

--- src/main/java/com/zhuoxin/whitelist/util/DatabaseInitializer.java
+++ src/main/java/com/zhuoxin/whitelist/util/DatabaseInitializer.java
@@ -50,24 +50,4 @@ public class DatabaseInitializer {
             }
         }
     }
-
-    // 创建 bind 表
-//    private static void createBindTable(Connection connection) throws SQLException {
-//        // 检查表是否存在
-//        DatabaseMetaData metaData = connection.getMetaData();
-//        ResultSet resultSet = metaData.getTables(null, null, "bind", null);
-//        if (!resultSet.next()) {
-//            // 表不存在，创建表
-//            String createTableSQL = "CREATE TABLE bind (" +
-//                    "name VARCHAR(20) NOT NULL," +
-//                    "source VARCHAR(20) DEFAULT NULL," +
-//                    "idNumber VARCHAR(100) DEFAULT NULL," +
-//                    "KEY fk_bind_name (name)," +
-//                    "CONSTRAINT fk_bind_name FOREIGN KEY (name) REFERENCES whitelist (name) ON DELETE CASCADE ON UPDATE CASCADE" +
-//                    ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb3";
-//            try (PreparedStatement statement = connection.prepareStatement(createTableSQL)) {
-//                statement.executeUpdate();
-//            }
-//        }
-//    }
 }
